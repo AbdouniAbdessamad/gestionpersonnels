fe55088a94b9e90d6db29cf4699c904bba612836
AbdouniAbdessamad <abdouni.abdou4@gmail.com>
2024-02-15 11:13:55 +0100

    simple language modifications

--- resources/views/gestionp/edit.blade.php
+++ resources/views/gestionp/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Modifier un gestionp') }}
+            {{ __('Modifier un personnel') }}
         </h2>
     </x-slot>
 
